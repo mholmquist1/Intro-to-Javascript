3f90132a512d3e73a6750ea6f6976c72e25cad91
M Holmquist <mai.holmquist@gmail.com>
2019-08-12 20:57:49 -0700

    day one practice.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function myFunk() {
+    let question = prompt('Tell me your name.');
+    console.log('Your name is ' + question);
+}
+
+
+
+console.log('I love to read');
+console.log('I love to hike');
+console.log('I love to swim');
+console.log('I love to cook');
+
+alert('Yo, welcome');
+
+
+// alert('OMG I like' + question + 'too!')console.log(alert);
+
+
+function grabElementAndSpitOutValue () {
+    //get element from index
+    const MAIN_DIV.innerHTML = 'hey, I m  here';
+}
