e6f73a2a7e3016e0a571cb507d67fc41cdcf551d
M Holmquist <mai.holmquist@gmail.com>
2019-08-14 20:52:08 -0700

    finished take home day 2.

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+//What is your first name?
+let firstName = prompt("What is your name?");
+
+
+//What is your last name?
+let lastName = prompt("What is your last name?");
+
+//Log in user's first name to console
+console.log(` ${firstName} `);
+console.log(` ${lastName}`);
+
+//Alert user's last firstName
+alert(`Hello ${lastName}`);
+
+//User's Birthday
+let birthday = prompt("What is your birthday?");
+
+//confirm birthday
+confirm(` Is your birthday? ${birthday}`);
+
+//Alert user's name on their birthday
+alert(`${new Date(birthday)} That is so awesome!!`);
+
+
+// const firstName = prommpt('What is your first name?');
+
+// const lastName = prompt('What is your Last name?');
+
+
+// //birthday
+// const birthDay = prompt()
