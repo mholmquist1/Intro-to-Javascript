5f7efc38a55391b1cb51dac9d3ede3958d6b5e27
M Holmquist <mai.holmquist@gmail.com>
2019-08-14 19:53:04 -0700

    class lessons that Jacque went over with us.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var firstName = 'Molly';
+// var age = 26;
+
+
+// //old way
+// console.log(' My name is ' + firstName + ' and my age is' + age );
+
+
+// //better way
+// console.log(` My name is ${firstName} and my age is ${age}`);
+
+// alert(`Hello, I am ${firstName}`);
+
+// let hello = new String('Hello, there.');
+// alert(hello);
+
