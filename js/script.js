e582dcff7af370ea395fa8796e7e5654a8dae4a8
M Holmquist <mai.holmquist@gmail.com>
2019-08-21 20:26:32 -0700

    need to clean up this code

---
+++ js/script.js
@@ -0,0 +1,106 @@
+// const greeting = "hello";
+
+// if(greeting === "hi") {
+//     console.log(`Yes, this matches to HI`);
+// } else if(greeting === "hello") {
+//     console.log(`Yes, this matched HELLO`);
+// } else if(greeting === 1) {
+//     console.log(`Yeah this matches an Integer of one`);
+// } else {
+//     console.log(`none of them matched.`);
+// }
+
+// const favColor = prompt(`Chose one of the following colors: red, grey, green`).toLowerCase();
+
+// switch(favColor) {
+//     case `red`:
+//     case `Red`:
+//         alert(`You chose ${favColor}.`);
+//     break;
+//     case `grey`:
+//     case `Grey`:
+//         alert(`You chose ${favColor}.`);
+//     break;
+//     default(`Sorry, you did not follow direction.`);
+// }
+
+// function changeToLowerCase(value) {
+//         return value.toLowerCase();
+// }
+
+// console.log(changeToLowerCase{HI});
+
+// function getWeatherReport(day, time, temperature) {
+//     const correctDayFormat = day.toUpperCase();
+//     const correctTimeFormat = time.toUpperCase();
+//     const correctTemperatureFormat = parseInt(temperature);
+//     const summary = `Today is ${correctDayFormat} and the time is ${correctTimeFormat}. The temperature is rockin ${correctTemperatureFormat}`;
+//     return summary;    
+// }
+
+//     const getsummary = this.getWeatherSummary(correctDayFormat, correctTimeFormat, correctTemperatureFormat);
+
+// function getWeatherSummary(day, time, temperature) {
+//     const summary = `Today is ${day} and the time is ${time}. The temperature is rockin ${temperature} degrees.`;
+// }
+
+// console.log(getWeatherReport{'wednesday', '6:00PM', '90'});
+
+// let myArrayOfRandomInfo = [
+//     'Edwards Scissorhands',
+//     7,
+//     true,
+//     '2005'
+// ];
+
+// console.log(myArrayOfRandomInfo.length);
+// console.log(myArrayOfRandomInfo[3]);
+// console.log(Array.isArray(myArrayOfRandomInfo));
+// myArrayOfRandomInfo.push(`I like Waterpolo`);
+// console.log(myArrayOfRandomInfo.length);
+// myArrayOfRandomInfo.unshift('I am now number one');
+// // myArrayOfRandomInfo.pop();
+// // console.log(myArrayOfRandomInfo);
+// // myArrayOfRandomInfo.splice(1, 1, 5);
+// // let newArrayofInfo = myArrayOfRandomInfo.slice(1);
+// // console.log(myArrayOfRandomInfo);
+// // newArrayofInfo.indexOf(`2011`);
+// // console.log(newArrayofInfo.indexOf(`2011`));
+
+// //5 random arrays
+
+// // let arrayOne = [
+// //     'Green',
+// //     'Shrub'
+// // ];
+
+// // let arrayTwo = [
+// //     'Red',
+// //     'Firetruck'
+// // ];
+
+// // // let finalArray = [
+// // //     arrayOne,
+// // //     arrayTwo
+// // // ];
+
+// // // console.log(finalArray[0][0]);
+// // console.log('hi')
+// // console.log(`yes there`)
+
+let favoriteMovie = [
+    'Click',
+    'Adam Sandler'
+];
+
+let favoriteMovieTwo = [
+    'The Avenger',
+    'Iron Man'
+]
+
+let favoriteMovieThree = [
+    'Wonder Woman',
+    'Wonder Woman'
+]
+console.log(favoriteMovie);
+console.log(favoriteMovieTwo);
