9b950007572edbd63c93345ac16bb99238622293
M Holmquist <mai.holmquist@gmail.com>
2019-09-01 15:28:40 -0700

    accidentally saved it on master. Created this new branch for the work.

---
+++ js/script.js
@@ -0,0 +1,91 @@
+
+// //anon function
+
+// let example = function () {
+//         console.log('hello there');
+// }
+
+// //name function
+
+// function exampleName() {
+//         console.log('hello there');
+// }
+
+// // example();
+
+// // const turnValueUppercase = val => console.log(val.toUpperCase());
+// // const multiplyValues = (arg1, arg2) => {return arg1 * arg2};
+
+// // turnValueUppercase('hello');
+// // console.log(multiplyValues(2,6));
+
+// // function turnValueUppercase(val) {
+// //         console.log(val.toUpperCase());
+// // }
+
+// //create an array of three names
+
+// let names = [
+//         'Molly',
+//         'Chloe',
+//         'Elloise',
+//         'Crystal'
+// ];
+// //create a greeting that will be taking in a name value
+// const greeting = name => `Good evening, ${name}`;
+
+// //test result first:
+// console.log(`The array of names ${names}`, `greeting function value: ${greeting('Example')}`);
+
+
+// const limit = 3;
+// //loop through names and add greeting function to it
+// for(j = 0; j < names.length; j++) {
+//         console.log(greeting(names[j]));
+// }
+
+//grab element from HTML;;
+const h1 = document.getElementById('greeting');
+const groceryList =
+document.getElementById('grocerylist'):
+
+const groceryItem = document.getElementById('groceryItem');
+
+const getGroceryItem = document.getElementById('getGroceryItem');
+
+let arrayOfGroceryItems = [];
+
+greeting.innerHTML = 'hello there';
+
+//create function to add onto grocery list
+
+// const addGroceryItem = (arr, value) => arr.push(value);
+// console.log(addGroceryItem(arrayOfGroceryItems, 'Carrots'));
+// console.log(arrayOfGroceryItems);
+
+// function addGroceryItems(arr, value) {
+//         arr.push(value);
+// }
+
+// function ejectGroceryItemToList(val) {
+//         //grab list item
+//         groceryItem.innerHTML = val;
+//         for(grocery = 0; grocery < arrayOfGroceryItems.length;grocery++) {
+
+//         }
+// }
+
+// ejectGroceryItemToList('testing');
+
+
+// function ejectGroceryItemToList(val);
+
+// for(grocery = 0; grocery < arrayOfGroceryItems.length; grocery++) {
+//         groceryList.append(testList)
+//         // groceryItem.innerHTML = val;
+// }
+
+// addGroceryItem(arrayOfGroceryItems, 'Testing');
+// addGroceryItem(arrayOfGroceryItems, 'Chocolate');
+// console.log(arrayOfGroceryItems);
+// ejectGroceryItemToList(arrayOfGroceryItems);
