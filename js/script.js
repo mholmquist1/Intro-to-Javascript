003e8a00391fe4a2fa4afbb1253845e2483a32a7
M Holmquist <mai.holmquist@gmail.com>
2019-09-01 16:32:29 -0700

    need to finish the css part for temperature

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+
+function celciusToFahrenheit() {
+    const cTempt = parseFloat(celciusInput.value);
+}
